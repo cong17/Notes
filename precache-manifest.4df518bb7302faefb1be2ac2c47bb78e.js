c9279379d3131b69b1f32d9ac063689ac59a27d4
congkxtb98 <congkxtb98@gmail.com>
2020-08-28 17:45:12 +0700

    Updates

---
+++ precache-manifest.4df518bb7302faefb1be2ac2c47bb78e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9004bbe60cac707c23e9ceb6a6d8e32",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "05c7f4bed05475991431",
+    "url": "/Notes/static/css/main.26889e9e.chunk.css"
+  },
+  {
+    "revision": "3f2eb1d170f722b8321b",
+    "url": "/Notes/static/js/2.6643a656.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Notes/static/js/2.6643a656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c7f4bed05475991431",
+    "url": "/Notes/static/js/main.32aa3dd7.chunk.js"
+  },
+  {
+    "revision": "fee180327d059e2b4996",
+    "url": "/Notes/static/js/runtime-main.3f765d47.js"
+  }
+]);
